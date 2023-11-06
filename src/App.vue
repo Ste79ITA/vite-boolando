d72170ea740cde9be3ad306b057de8e616782c92
Ste79ITA <nastri.s@gmail.com>
2023-11-06 15:33:54 +0100

    set components

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import PageHeader from './components/Page-Header.vue';
+import PageMain from './components/Page-Main.vue';
+import PageFooter from './components/Page-Footer.vue';
+
+export default {
+  components: {
+    PageHeader,
+    PageMain,
+    PageFooter,
+  },
+  data() {
+    return {
+      message: 'TEST',
+    };
+  },
+};
+</script>
+
+<template>
+  <PageHeader />
+  <PageMain />
+  <PageFooter />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/Page-Footer.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  data() {
+    return { message: 'TEST' };
+  },
+};
+</script>
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<style lang="scss" scoped></style>
